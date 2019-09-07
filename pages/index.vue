1fde98186fd270a06094c882c868e1452f13185d
nico <nico.mathar@gmail.com>
2019-09-08 01:40:07 +0200

    add actions to frontend

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,18 @@
 <template>
   <div class="container">
     <div>
-      <logo />
       <h1 class="title">
-        BackApp-Mongo --> {{message}}
+        Cron Status --> {{cron_status}}
       </h1>
       <h2 class="subtitle">
         Backup your MongoDB Databases
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <b-button variant="warning" @click="restartCron">Restart</b-button>
+      <b-button variant="success" @click="startCron" :disabled="cron_status === 'running'">Start</b-button>
+      <b-button variant="danger" @click="stopCron" :disabled="cron_status === 'stopped'" >Stop</b-button>
+      <b-alert v-if="message.length > 0" v-model="message" variant="warning" dismissible>
+        {{message}}
+      </b-alert>
     </div>
   </div>
 </template>
@@ -32,8 +21,42 @@
     export default {
         data(){
             return {
-                message : "Test1"
+                message : "",
+                cron_status: "stopped"
             }
+        },
+        methods: {
+            async getCronStatus() {
+                const {status} = await this.$axios.$get('api/cron/status')
+                this.cron_status = status
+            },
+            async restartCron(){
+                const {success, message} = await this.$axios.$get('api/cron/restart')
+                if(success){
+                    this.getCronStatus()
+                }else{
+                    this.message = message
+                }
+            },
+            async startCron(){
+                const {success, message} = await this.$axios.$get('api/cron/start')
+                if(success){
+                    this.getCronStatus()
+                }else{
+                    this.message = message
+                }
+            },
+            async stopCron(){
+                const {success, message} = await this.$axios.$get('api/cron/stop')
+                if(success){
+                    this.getCronStatus()
+                }else{
+                    this.message = message
+                }
+            }
+        },
+        mounted() {
+          this.getCronStatus()
         }
     }
 </script>
