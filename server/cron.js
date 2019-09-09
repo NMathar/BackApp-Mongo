1f534bbe69bf88bc14a34c079730b1380df06e8b
Nicolai Mathar <nicolai.mathar@ima.rwth-aachen.de>
2019-09-09 08:59:28 +0200

    add default cron to run job even if no data is present

--- server/cron.js
+++ server/cron.js
@@ -7,37 +7,42 @@ const _secretKey = "some-unique-key"; //TODO: add to config
 const simpleCrypto = new SimpleCrypto(_secretKey);
 
 let data = Backup
-  .value()
+    .value()
+
+// dummy schedule to let process start
+cron.schedule('*/5 * * * *', function () {
+    // console.log('Cron is running');
+});
 
 data.forEach(function (backup) {
-  console.log(backup);
-  let commandString = createDumpCommand({
-    database: backup.database,
-    host: 'mongodb://' + backup.hostname + ':' + backup.port,
-    dist: './temp/' + backup.database,
-    username: backup.username ? backup.username : '',
-    password: backup.password ? simpleCrypto.decrypt(backup.password) : '',
-    collections: backup.collections,
-    authenticationDatabase: backup.authenticationDatabase ? backup.authenticationDatabase : '',
-  });
-  console.log(commandString);
-  cron.schedule(backup.schedule, function () {
-    console.log("Run Backup for " + backup.hostname + " DB: " + backup.collection);
-    //TODO: handle backup.max_dumps
+    console.log(backup);
+    let commandString = createDumpCommand({
+        database: backup.database,
+        host: 'mongodb://' + backup.hostname + ':' + backup.port,
+        dist: './temp/' + backup.database,
+        username: backup.username ? backup.username : '',
+        password: backup.password ? simpleCrypto.decrypt(backup.password) : '',
+        collections: backup.collections,
+        authenticationDatabase: backup.authenticationDatabase ? backup.authenticationDatabase : '',
+    });
+    console.log(commandString);
+    cron.schedule(backup.schedule, function () {
+        console.log("Run Backup for " + backup.hostname + " DB: " + backup.collection);
+        //TODO: handle backup.max_dumps
 
-    // executeCommand(commandString, function (out) {
-    //   console.log("OUT", out);
-    // }, function (err) {
-    //   console.log("ERROR", err);
-    // })
-    //   .then(function (result) {
-    //     console.log(result);
-    //   })
-    //   .catch(function (error) {
+        // executeCommand(commandString, function (out) {
+        //   console.log("OUT", out);
+        // }, function (err) {
+        //   console.log("ERROR", err);
+        // })
+        //   .then(function (result) {
+        //     console.log(result);
+        //   })
+        //   .catch(function (error) {
 
-    //TODO: send mail on every fail to configured address
+        //TODO: send mail on every fail to configured address
 
-    //     throw error;
-    //   })
-  });
+        //     throw error;
+        //   })
+    });
 });
