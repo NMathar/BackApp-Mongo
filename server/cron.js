2f10e71988a41f1eadb5ef01ce4e9d85a44cd049
Nicolai Mathar <nicolai.mathar@ima.rwth-aachen.de>
2019-09-09 12:19:45 +0200

    fix cron

--- server/cron.js
+++ server/cron.js
@@ -3,26 +3,20 @@ const Backup = require('./model/backup.js');
 const createDumpCommand = require('ale-mongoutils').createDumpCommand;
 const {exec} = require('child_process');
 const SimpleCrypto = require("simple-crypto-js").default;
-const simpleCrypto = new SimpleCrypto(_secretKey);
+
 const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
     "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
 ];
 let data = Backup
     .value()
-
-const backupDir = __dirname + '/db/dumps/'//TODO: add to config
+const backupDir = __dirname + '/db/dumps/';//TODO: add to config
 const _secretKey = "some-unique-key"; //TODO: add to config
+const simpleCrypto = new SimpleCrypto(_secretKey);
 
 // dummy schedule to let process start
 cron.schedule('*/30 * * * *', function () {
     // console.log('Cron is running');
 });
-
-// exec('mongodump', (err, stdout, stderr) => {
-//     console.log(err)
-//     console.log(stderr)
-//     console.log(stdout)
-// });
 
 data.forEach(function (backup) {
     // console.log(backup);
@@ -67,11 +61,12 @@ function deleteIfMaxDumpsReached(id, max_dumps) {
 
     let folderArr = readdirSync(backupDir + id)
 
-    if(folderArr >= max_dumps){
+    if(folderArr.length > max_dumps){
         let sort = folderArr.sort(function (a, b) {
             return statSync(backupDir + id + '/' + a).mtime.getTime() - statSync(backupDir + id + '/' + b).mtime.getTime();
         });
         let delArray = sort.slice(0, sort.length - max_dumps)
+        // console.log(delArray)
         delArray.forEach(folder => {
             rimraf.sync(backupDir+id+'/'+folder)
         });
