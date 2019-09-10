44f8a3c12820bf4df65a057c86b1fb5ae549879d
Nicolai Mathar <nicolai.mathar@ima.rwth-aachen.de>
2019-09-10 14:15:19 +0200

    rm file creation

--- server/model/backup.js
+++ server/model/backup.js
@@ -5,12 +5,7 @@ const FileSync = require('lowdb/adapters/FileSync')
 const settingFile = __dirname + '/../db/settings.json'
 
 // create if file not exists
-fs.closeSync(fs.openSync(settingFile, 'a'));
-
-// fs.closeSync(fs.open(settingFile, 'a', (err) =>{
-//     console.log(err)
-//     fs.appendFileSync(settingFile, '{backups: []}', 'utf8');
-// }))
+// fs.closeSync(fs.openSync(settingFile, 'a'));
 
 const adapter = new FileSync(settingFile)
 const db = low(adapter)
