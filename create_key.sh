ce25e4564a7e9cd0c7fdc1e57b5a803d0b0e06f2
nico <nico.mathar@gmail.com>
2020-04-10 19:42:54 +0200

    fix add -p to dot env

--- create_key.sh
+++ create_key.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 FILE=server/db/.env
 if [ ! -f "$FILE" ]; then
-    string1=$(echo -n SECRET_KEY=)
+    string1=$(echo SECRET_KEY=)
     string2=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1)
     keyString="$string1$string2"
     echo $keyString > $FILE
