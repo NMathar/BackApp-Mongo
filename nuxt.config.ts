08d7de8ea18b870e1c3ebb63f36a8c1dec56bae2
nico <nico.mathar@gmail.com>
2020-04-12 11:24:13 +0200

    fix title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ const config: Configuration = {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || "",
+    title: "MongoDB Backup App",
     meta: [
       {charset: "utf-8"},
       {name: "viewport", content: "width=device-width, initial-scale=1"},
